2b6f46f8b433bf472ccacca3e9e602eddcdcb7ac
tails5555 <tails5555@naver.com>
2018-07-24 18:46:40 +0900

    Create Profile Image Component 프로필 사진에 대한 크기 변경을 반응형으로 받아서 React 생명 주기에 반영되는 Component 생성 완료. 사용자 로그인이 변경될 때 props의 상태가 바뀔 때 효과적으로 사용이 가능해 프로필을 불러올 때 굳이 Redux를 이용할 필요까진 없는 점에서 장점이 나옴.

---
+++ src/component/main_side/profile_image/UserProfile.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import defaultProfile from '../../resource_image/default_profile.png';
+import axios from "axios";
+
+const RESOURCE_ROOT_URL = 'http://127.0.0.1:8081/UserAPI/auth/resource/profile';
+
+class UserProfile extends Component{
+    constructor(props){
+        super();
+        this.state = { status : 0 };
+    }
+
+    componentWillReceiveProps(nextProps){
+        console.log(this.props.loginId);
+        console.log(nextProps.loginId);
+        if (this.props.loginId !== nextProps.loginId) {
+            this.propsUpdating(nextProps.loginId);
+        }
+    }
+
+    propsUpdating(loginId){
+        let self = this;
+        axios.get(`${RESOURCE_ROOT_URL}/image_profile/${loginId}`)
+            .then(response =>
+                self.setState({ status : response.status })
+            );
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+        return this.state.status !== nextState.status || nextProps.loginId !== this.props.loginId;
+    }
+
+    componentDidMount(){
+        let self = this;
+        axios.get(`${RESOURCE_ROOT_URL}/image_profile/${this.props.loginId}`)
+            .then(response =>
+                self.setState({ status : response.status })
+            );
+    }
+
+    render(){
+        return (
+            <img src={(this.state.status === 200) ? `${RESOURCE_ROOT_URL}/image_profile/${this.props.loginId}` : defaultProfile} alt=""
+                 style={{
+                     width : '100%',
+                     height : 'auto'
+                 }}/>
+        )
+    }
+}
+export default UserProfile;
